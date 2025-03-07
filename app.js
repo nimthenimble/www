138de7e6b50640c3d2ec66de92774d3900a3a3d4
nimthenimble <nimthenimble@gmail.com>
2025-03-07 13:40:44 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Download CV
+    document.getElementById('download-cv')?.addEventListener('click', function() {
+        const link = document.createElement('a');
+        link.href = 'downloads/CV-Hugo-Urías.pdf';
+        link.download = 'CV-Hugo-Urias.pdf';
+        link.click();
+    });
+
+    // Download Recommendation Letter Amjad
+    document.getElementById('amjad-letter')?.addEventListener('click', function() {
+        const link = document.createElement('a');
+        link.href = 'downloads/letters/Amjad.pdf';
+        link.download = 'Hugo Urias Recommendation Letter - Amjad Masad.pdf';
+        link.click();
+    });
+
+    // Download Recommendation Letter Antoine
+    document.getElementById('antoine-letter')?.addEventListener('click', function() {
+        const link = document.createElement('a');
+        link.href = 'downloads/letters/Antoine.pdf';
+        link.download = 'Hugo Urias Recommendation Letter - Antoine Buteau.pdf';
+        link.click();
+    });
+
+    // Download Recommendation Letter Ian
+    document.getElementById('ian-letter')?.addEventListener('click', function() {
+        const link = document.createElement('a');
+        link.href = 'downloads/letters/Ian.pdf';
+        link.download = 'Hugo Urias Recommendation Letter - Ian Simpson.pdf';
+        link.click();
+    });
+})
